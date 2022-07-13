f17b5fc40b7d6b5ea3c8f5d85cbf5b5dc62ad13e
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-07-13 18:14:51 +0300

    add worker pool

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -14,5 +14,27 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	jobs := make(chan int64, n)
+	results := make(chan user, n)
+	users := make([]user, 0, n)
+
+	for w := int64(1); w <= pool; w++ {
+		go worker(jobs, results)
+	}
+
+	for j := int64(0); j < n; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	for a := int64(1); a <= n; a++ {
+		detestedUser := <-results
+		users = append(users, detestedUser)
+	}
+	return users
+}
+
+func worker(jobs <-chan int64, results chan<- user) {
+	for j := range jobs {
+		results <- getOne(j)
+	}
 }
